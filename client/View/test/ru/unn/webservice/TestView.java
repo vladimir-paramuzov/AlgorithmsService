4c8d6f15d35a69e8bb0c9d0fbce8592f6e1d37bf
Vladimir Paramuzov <vvp109@gmail.com>
2016-03-11 20:12:50 +0300

    Init project

---
+++ client/View/test/ru/unn/webservice/TestView.java
@@ -0,0 +1,4 @@
+package ru.unn.webservice;
+
+public class TestView {
+}
---
+++ server/Model/src/ru/unn/webservice/Main.java
@@ -0,0 +1,7 @@
+package ru.unn.webservice;
+
+public class Main {
+	public static void main(final String[] args) {
+		System.out.println("aaaaaaa");
+	}
+}
