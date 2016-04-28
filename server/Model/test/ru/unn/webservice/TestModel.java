da0ae0965b7aea5bc7bf78ee0b7a1ef10b9bff22
Gorokhov Dmitriy <gorokhovdmit@gmail.com>
2016-04-28 22:37:14 +0300

    Check possibility to push into rep

--- server/Model/test/ru/unn/webservice/TestModel.java
+++ server/Model/test/ru/unn/webservice/TestModel.java
@@ -1,4 +1,18 @@
 package ru.unn.webservice;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class TestModel {
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(true, true);
+    }
 }
