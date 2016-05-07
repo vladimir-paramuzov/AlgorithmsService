e7b9a6a3e17461be2d13c4b952e33e99f8fda26c
MolotkovAndrei <molotkov.and@gmail.com>
2016-05-07 16:14:34 +0300

    added interface

--- client/View/src/ru/unn/webservice/client/IViewModel.java
+++ client/View/src/ru/unn/webservice/client/IViewModel.java
@@ -1,4 +1,13 @@
 package ru.unn.webservice.client;
 
+import java.util.List;
+
 public interface IViewModel {
+    boolean isSearchButtonEnabled();
+    void setTag(final String tag);
+
+    String getTag();
+    List getListAlgorithms();
+
+    void searchAlgorithm();
 }
--- client/View/src/ru/unn/webservice/client/View.java
+++ client/View/src/ru/unn/webservice/client/View.java
@@ -4,16 +4,50 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public final class View {
+    private IViewModel viewModel;
     private JPanel mainPanel;
+    private JPanel viewPanel;
+    private JPanel settingsPanel;
+    private JTextField tagTextField;
+    private JButton searchButton;
+    private JButton superUserButton;
+    private JButton logOutButton;
+    private JButton addAlgorithmButton;
+    private JButton giveUsYourMoneyButton;
+    private JButton authorizationButton;
     private JButton registerButton;
+    private JList algorithmsList;
+    private JLabel balanceLabel;
+    private JLabel userTypeLabel;
 
     private View() {
 
     }
 
     private View(final IViewModel viewModel) {
+        this.viewModel = viewModel;
+        bind();
 
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                backBind();
+                viewModel.searchAlgorithm();
+                bind();
+            }
+        });
     }
+
+    private void bind() {
+        searchButton.setEnabled(viewModel.isSearchButtonEnabled());
+        algorithmsList.setListData(viewModel.getListAlgorithms().toArray());
+        tagTextField.setText(viewModel.getTag());
+    }
+
+    private void backBind() {
+        viewModel.setTag(tagTextField.getText());
+    }
+
     public static void main(final String[] args) {
         JFrame frame = new JFrame("Algorithm webservice client");
 
